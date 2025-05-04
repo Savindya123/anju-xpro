fd00c6d71eb013cb5e7415cf47a56279708fd571
Savindya123 <136538677+Savindya123@users.noreply.github.com>
2025-05-04 21:52:05 +0530

    Update session.js

--- session.js
+++ session.js
@@ -40,7 +40,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "your session id",
+    SESSION_ID: process.env.SESSION_ID || "ANJU-XPRO~7JFTFbiQ#NgCzl7b3pbVSg3uCAXCKpDlkY8aW2ENeubAt2pZ1fbY",
     GITHUB_TOKEN: process.env.GITHUB_TOKEN || "github token without ghp_",
     GITHUB_USERNAME: process.env.GITHUB_USERNAME || "github username",
     SUDO: process.env.SUDO ? process.env.SUDO.split(",") : ["94717775628", "94758775628"],
